38f724a846f40370dc9b3b66697eaa655999a613
Hossein Shayan <hssnshayan@gmail.com>
2022-07-30 16:51:44 +0430

    feat: :zap: cart context is added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { Fragment, useState } from "react";
+import React, { useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   const [showModalCart, setShowModalCart] = useState(false);
@@ -13,13 +14,13 @@ function App() {
     setShowModalCart(false);
   };
   return (
-    <Fragment>
+    <CartProvider>
       { showModalCart && <Cart onHideModal={hideModalCartHandler}/>}
       <Header onShowModal={showModalCartHandler}/>
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
---
+++ src/store/CartProvider.js
@@ -0,0 +1,19 @@
+import React from "react";
+import CartContext from "./cart.context";
+
+const CartProvider = ({ children }) => {
+  const addItemToCart = (item) => {};
+  const removeItemFromCart = (id) => {};
+
+  const cartContext = {
+    items: [],
+    totalAmount: 0,
+    addItem: addItemToCart,
+    removeItem: removeItemFromCart,
+  };
+  return (
+    <CartContext.Provider value={cartContext}>{children}</CartContext.Provider>
+  );
+};
+
+export default CartProvider;
