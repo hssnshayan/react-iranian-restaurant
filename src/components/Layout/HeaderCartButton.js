8e065c2e0f0fbcce22be6904784f83b0cc48ca28
Hossein Shayan <hssnshayan@gmail.com>
2022-07-31 13:54:57 +0430

    feat: :fire: add to cart btn animation is added

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,37 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart.context";
 import CartIcon from "../Cart/CartIcon";
 import styles from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = ({ onClick }) => {
   const cartCtx = useContext(CartContext);
+  const { items } = cartCtx;
+  const [cartBtnEffect, setCartBtnEffect] = useState(false);
 
-  const numberOfCartItems = cartCtx.items.reduce((currentNumber, item) => {
+  const numberOfCartItems = items.reduce((currentNumber, item) => {
     return currentNumber + item.amount;
   }, 0);
 
+  const cartHeaderBottonClasses = `${styles.button} ${
+    cartBtnEffect ? styles.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (items.legth === 0) {
+      return;
+    }
+    setCartBtnEffect(true);
+    const timer = setTimeout(() => {
+      setCartBtnEffect(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={styles.button} onClick={onClick}>
+    <button className={cartHeaderBottonClasses} onClick={onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
